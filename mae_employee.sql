05a45e6d7c26108b0d2df43668b60f9f18792366
MayorTheDataQueen <mayowabeyioku@gmail.com>
2026-08-12 13:34:12 -0400

    Delete mae_employee.sql

--- mae_employee.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM mae_coffeehouse.mae_employee;
-
-SELECT * FROM mae_coffeehouse.mae_inventory;
-
-SELECT * FROM mae_coffeehouse.mae_coffee;
-
-SELECT * FROM mae_coffeehouse.maecoffeehouse_sales;
- 
